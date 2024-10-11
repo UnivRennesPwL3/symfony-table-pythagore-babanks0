a79efff592ba06b5578125ecebe10a1346dd8bad
Steph <charbel-stephane.bankole@etudiant.univ-rennes.fr>
2024-10-11 18:43:00 +0200

    Ajout du contrôleur Pythagore, service, et vue pour afficher la table de Pythagore

--- src/Service/PythagoreUtility.php
+++ src/Service/PythagoreUtility.php
@@ -8,13 +8,27 @@ class PythagoreUtility
     {
         $html = '<table border="1">'; // Début de la table avec une bordure
 
+        // Première ligne avec 0, 1, 2, ..., 10
+        $html .= '<tr><td>0</td>'; // Première case "0" en haut à gauche
+        for ($j = 1; $j <= 10; $j++) {
+            $html .= '<td>' . $j . '</td>'; // Insère 1 à 10 dans la première ligne
+        }
+        $html .= '</tr>';
+
+        // Boucle pour créer le reste des lignes
         for ($i = 1; $i <= 10; $i++) {
-            $html .= '<tr>'; // Début d'une nouvelle ligne
+            $html .= '<tr>';
+            // Insérer la première colonne avec les nombres de 1 à 10
+            $html .= '<td>' . $i . '</td>'; // Colonne gauche avec 1 à 10
+
             for ($j = 1; $j <= 10; $j++) {
                 if ($j == $i) {
+                    // Mettre "X" sur la diagonale
                     $html .= '<td>X</td>';
-                }else
-                $html .= '<td>' . ($i * $j) . '</td>'; // Multiplier et insérer dans une cellule
+                } else {
+                    // Multiplier et insérer dans une cellule pour les autres cases
+                    $html .= '<td>' . ($i * $j) . '</td>';
+                }
             }
             $html .= '</tr>'; // Fin de la ligne
         }
